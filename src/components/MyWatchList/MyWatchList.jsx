2302209d38e6f2bc4ffee771fafb3f1a1a5b7662
Denys Severin <severindenys367@gmail.com>
2025-01-08 15:09:58 +0100

    added saving and retrieving clicked movie to my watch list

--- src/components/MyWatchList/MyWatchList.jsx
+++ src/components/MyWatchList/MyWatchList.jsx
@@ -1,5 +1,31 @@
+import { useState, useEffect } from "react";
+
 const MyWatchList = () => {
-  return <div className="myWatchList-container">MyWatchList</div>;
+  const [watchList, setWatchList] = useState([]);
+
+  useEffect(() => {
+    const savedMovies = localStorage.getItem("savedMovies");
+    setWatchList(savedMovies ? JSON.parse(savedMovies) : []);
+  }, []);
+  return (
+    <>
+      <div className="myWatchList-container">
+        {watchList.length === 0 ? (
+          <p>No movies in your watchlist yet.</p>
+        ) : (
+          watchList.map((movie) => (
+            <div key={movie.imdbID} className="movie-item">
+              <img src={movie.Poster} alt={movie.Title} />
+              <h3>{movie.Title}</h3>
+              <p>Year: {movie.Year}</p>
+              <p>Rating: {movie.imdbRating}</p>
+            </div>
+          ))
+        )}
+      </div>
+      ;
+    </>
+  );
 };
 
 export default MyWatchList;
